e1f398b0b31fb5cb7c4607d2c8569f2bc9203a56
Jaai <arcticquests.dev@gmail.com>
2026-06-26 22:28:59 +0530

    Fix Broken stuff from merge

--- src/main/java/net/enorme/NER/worldgen/ModBiomeModifiers.java
+++ src/main/java/net/enorme/NER/worldgen/ModBiomeModifiers.java
@@ -1,18 +1,14 @@
 package net.enorme.NER.worldgen;
 
 import net.enorme.NER.NethersExorcismMod;
-import net.enorme.NER.worldgen.biome.NetherBiomeRegion;
 import net.minecraft.core.HolderGetter;
-import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.neoforged.neoforge.common.world.BiomeModifier;
-import net.neoforged.neoforge.common.world.BiomeModifiers;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 public class ModBiomeModifiers {
@@ -23,7 +19,7 @@ public class ModBiomeModifiers {
     public static void bootstrap(BootstrapContext<BiomeModifier> context) {
         HolderGetter<PlacedFeature> placedFeatures = context.lookup(Registries.PLACED_FEATURE);
         HolderGetter<Biome> biomes = context.lookup(Registries.BIOME);
-
+/*
         context.register(ADD_DNA_FOREST_TREES, new BiomeModifiers.AddFeaturesBiomeModifier(
                 HolderSet.direct(biomes.getOrThrow(NetherBiomeRegion.DNA_FOREST)),
                 HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.INDIGO_TREES)),
@@ -34,7 +30,7 @@ public class ModBiomeModifiers {
                 HolderSet.direct(biomes.getOrThrow(NetherBiomeRegion.DNA_FOREST)),
                 HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.INDIGO_VEGETATION)),
                 GenerationStep.Decoration.VEGETAL_DECORATION
-        ));
+        ));*/
     }
 
     private static ResourceKey<BiomeModifier> registerKey(String name) {
